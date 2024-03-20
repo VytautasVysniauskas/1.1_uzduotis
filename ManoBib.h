fe08fe6d87ca70be159006913a44c19458b0900e
VytautasVysniauskas <159624996+VytautasVysniauskas@users.noreply.github.com>
2024-03-20 12:25:30 +0200

    UpdateF ManoBib.h
    Prideti 2 struct konteineriai ir pakoreguotos funkcijos

--- ManoBib.h
+++ ManoBib.h
@@ -1,19 +1,42 @@
 #pragma once
 #include "Bibliotekos.h"
 using namespace std;
+using namespace std::chrono;
 
 struct mokiniai
 {
 	string name;
 	string surname;
+	vector<int> ndRez;
+	int egzRez;
 	double average;
 	double med;
 };
-void spauzdinimasFaile(mokiniai[], int);
-void spauzdinimasEkrane(mokiniai[], int);
-void vidurys(vector<vector<int>>&, vector<double>&);
-void skaitymasRanka(vector<string>&, vector<string>&, vector<vector<int>>&, vector<int>&, vector<double>&);
-int skaitymasFailo(vector<string>&, vector<string>&, vector<vector<int>>&, vector<int>&, vector<double>&);
-void rikiavimas(mokiniai[], int, char);
-void duomenuGeneravimas(int);
-void kiekGeneruoti();
+struct pazenge
+{
+	string name;
+	string surname;
+	vector<int> ndRez;
+	int egzRez;
+	double average;
+	double med;
+};
+struct buki
+{
+	string name;
+	string surname;
+	vector<int> ndRez;
+	int egzRez;
+	double average;
+	double med;
+};
+
+void spauzdinimasFaile(vector<mokiniai>&, int, duration<double>&, duration<double>&);
+void spauzdinimasEkrane(vector<mokiniai>&, int, duration<double>&);
+void vidurys(vector<mokiniai>&, int);
+int skaitymasRanka(vector<mokiniai>&, int);
+int skaitymasFailo(vector<mokiniai>&, int, duration<double>&, duration<double>&);
+void rikiavimas(vector<mokiniai>&, int, char);
+void pazangusIrBuki(vector<mokiniai>&, vector<pazenge>&, vector<buki>&, int, duration<double>&, duration<double>&, duration<double>&);
+int duomenuGeneravimas(vector<mokiniai>&, int, duration<double>&);
+int kiekGeneruoti(vector<mokiniai>&, int, duration<double>&, duration<double>&);
