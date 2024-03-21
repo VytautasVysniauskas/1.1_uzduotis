92f5b053c87f32244ce7222432793d2c12263de1
VytautasVysniauskas <159624996+VytautasVysniauskas@users.noreply.github.com>
2024-03-21 19:47:39 +0200

    Add files via upload

---
+++ vector/3_strategija/ManoBib.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Bibliotekos.h"
+using namespace std;
+using namespace std::chrono;
+
+struct mokiniai
+{
+	string name;
+	string surname;
+	vector<int> ndRez;
+	int egzRez;
+	double average;
+	double med;
+};
+
+void spauzdinimasFaile(vector<mokiniai>&, int);
+void spauzdinimasEkrane(vector<mokiniai>&, int);
+void vidurys(vector<mokiniai>&, int);
+int skaitymasRanka(vector<mokiniai>&, int);
+int skaitymasFailo(vector<mokiniai>&, int, duration<double>&);
+void rikiavimas(vector<mokiniai>&, int, char);
+void pazangusIrBuki(vector<mokiniai>&, vector<mokiniai>&, int, duration<double>&, duration<double>&);
+int duomenuGeneravimas(vector<mokiniai>&, int);
+int kiekGeneruoti(vector<mokiniai>&, int);
