f0f4200262054eee82e8f9bc48449667dba552a3
VytautasVysniauskas <159624996+VytautasVysniauskas@users.noreply.github.com>
2024-03-21 17:13:26 +0200

    Add files via upload

---
+++ vector/ManoBib.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "Bibliotekos.h"
+using namespace std;
+using namespace std::chrono;
+
+struct mokiniai
+{
+	string name;
+	string surname;
+	vector<int> ndRez;
+	int egzRez;
+	double average;
+	double med;
+};
+struct buki
+{
+	string name;
+	string surname;
+	vector<int> ndRez;
+	int egzRez;
+	double average;
+	double med;
+};
+
+void spauzdinimasFaile(vector<mokiniai>&, int);
+void spauzdinimasEkrane(vector<mokiniai>&, int);
+void vidurys(vector<mokiniai>&, int);
+int skaitymasRanka(vector<mokiniai>&, int);
+int skaitymasFailo(vector<mokiniai>&, int, duration<double>&);
+void rikiavimas(vector<mokiniai>&, int, char);
+void pazangusIrBuki(vector<mokiniai>&, vector<buki>&, int, duration<double>&, duration<double>&);
+int duomenuGeneravimas(vector<mokiniai>&, int);
+int kiekGeneruoti(vector<mokiniai>&, int);
---
+++ vector/v1.0.cpp
@@ -0,0 +1,80 @@
+#include "Bibliotekos.h"
+using namespace std;
+using namespace std::chrono;
+
+int main()
+{
+	auto laikas1 = duration<double>::zero();
+	auto laikas2 = duration<double>::zero();
+	auto laikas3 = duration<double>::zero();
+	int dydis = 0;
+	vector<mokiniai> M(1000000);
+	vector<buki> B(0);
+	char raide;
+
+	cout << "Ar norite generuoti duomenys?" << endl << "(T) - taip / (bet koks kitas simbolis) - ne" << endl;
+	cin >> raide;
+
+	if (raide == 'T' || raide == 't')
+	{
+		dydis = kiekGeneruoti(M, dydis);
+		pazangusIrBuki(M, B, dydis, laikas2, laikas3);
+		cout << "Sukurti 2 atskiri failai:\npazenge.txt ir buki.txt" << endl << endl;
+		M.clear();
+		M.resize(1000000);
+	}
+
+	cout << "Pasrinkite ar norite duomenys rasyti ranka, ar skaityti is failo. Rasykite atitinkama raide." << endl << "Rasyti ranka - (R) / Skaityti is failo - bet koks kitas zenklas" << endl;
+	cin >> raide;
+
+	if (raide == 'R' || raide == 'r')
+	{
+		dydis = skaitymasRanka(M, dydis);
+	}
+	else
+	{
+		dydis = skaitymasFailo(M, dydis, laikas1);
+		if (dydis == 0)
+		{
+			return 0;
+		}
+	}
+
+	vidurys(M, dydis);
+
+	cout << "Pasirinkite pagal ka norite surusiuoti rezultatus (Mazejimo tvarka arba abaceles didejimo)" << endl << "Vardus (V) / Pavardes (P) / Gal. Vidurki (A) / Gal. Mediana (bet koks kitas zenklas)" << endl;
+	cin >> raide;
+	if (raide == 'V' || raide == 'v')
+	{
+		rikiavimas(M, dydis, raide);
+	}
+	else if (raide == 'P' || raide == 'p')
+	{
+		rikiavimas(M, dydis, raide);
+	}
+	else if (raide == 'A' || raide == 'a')
+	{
+		rikiavimas(M, dydis, raide);
+	}
+	else
+	{
+		rikiavimas(M, dydis, raide);
+	}
+
+	cout << "Ar norite rezultata spauzdinti faile ar ekrane?" << endl << "Faile (F) / Ekrane (bet koks kitas zenklas)" << endl;
+	cin >> raide;
+
+	if (raide == 'E' || raide == 'e')
+	{
+		spauzdinimasEkrane(M, dydis);
+	}
+	else
+	{
+		spauzdinimasFaile(M, dydis);
+	}
+
+	cout << "\nDuomenu nuskaitymas is failo i konteineri: " << fixed << setprecision(5) << laikas1.count() << endl;
+	cout << "Duomenu rusiavimas atitinkamam konteinery: " << fixed << setprecision(5) << laikas2.count() << endl;
+	cout << "Duomenu skirtstymas i 2 konteinerius: " << fixed << setprecision(5) << laikas3.count() << endl;
+	return 0;
+}
