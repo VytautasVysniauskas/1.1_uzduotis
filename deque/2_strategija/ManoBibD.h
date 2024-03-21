c9fc0d77c5e20e923837d69c169091973d7a6eec
VytautasVysniauskas <159624996+VytautasVysniauskas@users.noreply.github.com>
2024-03-21 17:12:18 +0200

    Add files via upload

---
+++ deque/2_strategija/ManoBibD.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "BibliotekosD.h"
+using namespace std;
+using namespace std::chrono;
+
+struct mokiniai
+{
+	string name;
+	string surname;
+	deque<int> ndRez;
+	int egzRez;
+	double average;
+	double med;
+};
+struct pazenge
+{
+	string name;
+	string surname;
+	deque<int> ndRez;
+	int egzRez;
+	double average;
+	double med;
+};
+struct buki
+{
+	string name;
+	string surname;
+	deque<int> ndRez;
+	int egzRez;
+	double average;
+	double med;
+};
+
+void spauzdinimasFaile(deque<mokiniai>&, int);
+void spauzdinimasEkrane(deque<mokiniai>&, int);
+void vidurys(deque<mokiniai>&, int);
+int skaitymasRanka(deque<mokiniai>&, int);
+int skaitymasFailo(deque<mokiniai>&, int, duration<double>&);
+void rikiavimas(deque<mokiniai>&, int, char);
+void pazangusIrBuki(deque<mokiniai>&, deque<buki>&, int, duration<double>&, duration<double>&);
+int duomenuGeneravimas(deque<mokiniai>&, int, duration<double>&);
+int kiekGeneruoti(deque<mokiniai>&, int);
