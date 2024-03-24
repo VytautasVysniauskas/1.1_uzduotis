32485f98bfacec7d6058ff7a14be2bd262075af3
VytautasVysniauskas <159624996+VytautasVysniauskas@users.noreply.github.com>
2024-03-24 10:33:32 +0200

    Update ManoBibD.h
    Kodas atitinkamai pakeistas

--- deque/3_strategija/ManoBibD.h
+++ deque/3_strategija/ManoBibD.h
@@ -13,12 +13,11 @@ struct mokiniai
 	double med;
 };
 
-void spauzdinimasFaile(deque<mokiniai>&, int);
+void spauzdinimasFaile(deque<mokiniai>&, deque<mokiniai>&, int, duration<double>&);
 void spauzdinimasEkrane(deque<mokiniai>&, int);
 void vidurys(deque<mokiniai>&, int);
 int skaitymasRanka(deque<mokiniai>&, int);
 int skaitymasFailo(deque<mokiniai>&, int, duration<double>&);
 void rikiavimas(deque<mokiniai>&, int, char);
-void pazangusIrBuki(deque<mokiniai>&, deque<mokiniai>&, int, duration<double>&, duration<double>&);
-int duomenuGeneravimas(deque<mokiniai>&, int, duration<double>&);
-int kiekGeneruoti(deque<mokiniai>&, int);
+void duomenuGeneravimas(int);
+void kiekGeneruoti();
