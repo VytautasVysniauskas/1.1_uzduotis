324e0ca8fe3f4aaee5d433ebc0dcbe3c4652bc59
VytautasVysniauskas <159624996+VytautasVysniauskas@users.noreply.github.com>
2024-03-24 10:33:08 +0200

    Update dv1.0.cpp
    Kodas atitinkamai pakeistas

--- deque/3_strategija/dv1.0.cpp
+++ deque/3_strategija/dv1.0.cpp
@@ -9,7 +9,7 @@ int main()
 	auto laikas3 = duration<double>::zero();
 	auto pradzia = steady_clock::now();
 	int dydis = 0;
-	deque<mokiniai> M(0);
+	deque<mokiniai> M(1000000);
 	deque<mokiniai> B(0);
 	char raide;
 
@@ -18,11 +18,8 @@ int main()
 
 	if (raide == 'T' || raide == 't')
 	{
-		dydis = kiekGeneruoti(M, dydis);
-		pazangusIrBuki(M, B, dydis, laikas2, laikas3);
-		cout << "Sukurti 2 atskiri failai:\npazenge.txt ir buki.txt" << endl << endl;
-		M.clear();
-		M.resize(1000000);
+		kiekGeneruoti();
+
 	}
 
 	cout << "Pasrinkite ar norite duomenys rasyti ranka, ar skaityti is failo. Rasykite atitinkama raide." << endl << "Rasyti ranka - (R) / Skaityti is failo - bet koks kitas zenklas" << endl;
@@ -37,6 +34,7 @@ int main()
 		dydis = skaitymasFailo(M, dydis, laikas1);
 		if (dydis == 0)
 		{
+			cout << "Nepavyko nuskaityti duomenu, patikrinkite savo duomenu faila ir bandykite vel." << endl;
 			return 0;
 		}
 	}
@@ -45,22 +43,10 @@ int main()
 
 	cout << "Pasirinkite pagal ka norite surusiuoti rezultatus (Mazejimo tvarka arba abaceles didejimo)" << endl << "Vardus (V) / Pavardes (P) / Gal. Vidurki (A) / Gal. Mediana (bet koks kitas zenklas)" << endl;
 	cin >> raide;
-	if (raide == 'V' || raide == 'v')
-	{
-		rikiavimas(M, dydis, raide);
-	}
-	else if (raide == 'P' || raide == 'p')
-	{
-		rikiavimas(M, dydis, raide);
-	}
-	else if (raide == 'A' || raide == 'a')
-	{
-		rikiavimas(M, dydis, raide);
-	}
-	else
-	{
-		rikiavimas(M, dydis, raide);
-	}
+	auto pradzia2 = steady_clock::now();
+	rikiavimas(M, dydis, raide);
+	auto pabaiga2 = steady_clock::now();
+	laikas2 = duration_cast<duration<double>>(pabaiga2 - pradzia2);
 
 	cout << "Ar norite rezultata spauzdinti faile ar ekrane?" << endl << "Faile (F) / Ekrane (bet koks kitas zenklas)" << endl;
 	cin >> raide;
@@ -71,7 +57,8 @@ int main()
 	}
 	else
 	{
-		spauzdinimasFaile(M, dydis);
+		spauzdinimasFaile(M, B, dydis, laikas3);
+		cout << endl << "Sukurti 2 rez failai: pazenge.txt ir buki.txt" << endl;
 	}
 
 
