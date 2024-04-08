1ac68df6f896565be14a68f0134e5ee505fe745d
VytautasVysniauskas <159624996+VytautasVysniauskas@users.noreply.github.com>
2024-04-09 01:30:57 +0300

    Update ManoBib.h

--- vector/2_strategija/ManoBib.h
+++ vector/2_strategija/ManoBib.h
@@ -12,22 +12,12 @@ struct mokiniai
 	double average;
 	double med;
 };
-struct buki
-{
-	string name;
-	string surname;
-	vector<int> ndRez;
-	int egzRez;
-	double average;
-	double med;
-};
 
-void spauzdinimasFaile(vector<mokiniai>&, int);
+void spauzdinimasFaile(vector<mokiniai>&, int, vector<mokiniai>&, int);
 void spauzdinimasEkrane(vector<mokiniai>&, int);
 void vidurys(vector<mokiniai>&, int);
 int skaitymasRanka(vector<mokiniai>&, int);
 int skaitymasFailo(vector<mokiniai>&, int, duration<double>&);
 void rikiavimas(vector<mokiniai>&, int, char);
-void pazangusIrBuki(vector<mokiniai>&, vector<buki>&, int, duration<double>&, duration<double>&);
 int duomenuGeneravimas(vector<mokiniai>&, int);
 int kiekGeneruoti(vector<mokiniai>&, int);
