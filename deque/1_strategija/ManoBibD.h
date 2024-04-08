daa81e73c1ba2f7979cfbcaaf6e05138cba5d52f
VytautasVysniauskas <159624996+VytautasVysniauskas@users.noreply.github.com>
2024-04-09 02:11:50 +0300

    Update ManoBibD.h

--- deque/1_strategija/ManoBibD.h
+++ deque/1_strategija/ManoBibD.h
@@ -12,31 +12,12 @@ struct mokiniai
 	double average;
 	double med;
 };
-struct pazenge
-{
-	string name;
-	string surname;
-	deque<int> ndRez;
-	int egzRez;
-	double average;
-	double med;
-};
-struct buki
-{
-	string name;
-	string surname;
-	deque<int> ndRez;
-	int egzRez;
-	double average;
-	double med;
-};
 
-void spauzdinimasFaile(deque<mokiniai>&, int, duration<double>&, duration<double>&);
+void spauzdinimasFaile(deque<mokiniai>&, deque<mokiniai>&, deque<mokiniai>&, int, duration<double>&, duration<double>&);
 void spauzdinimasEkrane(deque<mokiniai>&, int, duration<double>&);
 void vidurys(deque<mokiniai>&, int);
 int skaitymasRanka(deque<mokiniai>&, int);
 int skaitymasFailo(deque<mokiniai>&, int, duration<double>&, duration<double>&);
 void rikiavimas(deque<mokiniai>&, int, char);
-void pazangusIrBuki(deque<mokiniai>&, deque<pazenge>&, deque<buki>&, int, duration<double>&, duration<double>&, duration<double>&);
 int duomenuGeneravimas(deque<mokiniai>&, int, duration<double>&);
 int kiekGeneruoti(deque<mokiniai>&, int, duration<double>&, duration<double>&);
