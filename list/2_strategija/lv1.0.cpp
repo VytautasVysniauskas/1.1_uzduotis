a5c9d81a3f359945476262ed8f0a3cef93ae21cf
VytautasVysniauskas <159624996+VytautasVysniauskas@users.noreply.github.com>
2024-04-09 01:53:17 +0300

    Update lv1.0.cpp

--- list/2_strategija/lv1.0.cpp
+++ list/2_strategija/lv1.0.cpp
@@ -9,7 +9,7 @@ int main()
 	auto laikas3 = duration<double>::zero();
 	int dydis = 0;
 	list<mokiniai> M(1000000);
-	list<buki> B(0);
+	list<mokiniai> B(0);
 	char raide;
 
 	cout << "Ar norite generuoti duomenys?" << endl << "(T) - taip / (bet koks kitas simbolis) - ne" << endl;
@@ -18,8 +18,6 @@ int main()
 	if (raide == 'T' || raide == 't')
 	{
 		dydis = kiekGeneruoti(M, dydis);
-		pazangusIrBuki(M, B, dydis, laikas2, laikas3);
-		cout << "Sukurti 2 atskiri failai:\npazenge.txt ir buki.txt" << endl << endl;
 		M.clear();
 		M.resize(1000000);
 	}
@@ -44,22 +42,8 @@ int main()
 
 	cout << "Pasirinkite pagal ka norite surusiuoti rezultatus (Mazejimo tvarka arba abaceles didejimo)" << endl << "Vardus (V) / Pavardes (P) / Gal. Vidurki (A) / Gal. Mediana (bet koks kitas zenklas)" << endl;
 	cin >> raide;
-	if (raide == 'V' || raide == 'v')
-	{
-		rikiavimas(M, dydis, raide);
-	}
-	else if (raide == 'P' || raide == 'p')
-	{
-		rikiavimas(M, dydis, raide);
-	}
-	else if (raide == 'A' || raide == 'a')
-	{
-		rikiavimas(M, dydis, raide);
-	}
-	else
-	{
-		rikiavimas(M, dydis, raide);
-	}
+
+	rikiavimas(M, dydis, raide);
 
 	cout << "Ar norite rezultata spauzdinti faile ar ekrane?" << endl << "Faile (F) / Ekrane (bet koks kitas zenklas)" << endl;
 	cin >> raide;
@@ -69,7 +53,7 @@ int main()
 	}
 	else
 	{
-		spauzdinimasFaile(M, dydis);
+		spauzdinimasFaile(M, B, dydis);
 	}
 
 	cout << "\nDuomenu is failo i konteineri nuskaitymas: " << fixed << setprecision(5) << laikas1.count() << endl;
