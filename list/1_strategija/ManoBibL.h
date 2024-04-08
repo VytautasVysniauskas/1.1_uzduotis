4acbeaad1bccf1e6f23d2158cf0b4bfb42709545
VytautasVysniauskas <159624996+VytautasVysniauskas@users.noreply.github.com>
2024-04-09 01:45:06 +0300

    Update ManoBibL.h

--- list/1_strategija/ManoBibL.h
+++ list/1_strategija/ManoBibL.h
@@ -12,31 +12,12 @@ struct mokiniai
 	double average;
 	double med;
 };
-struct pazenge
-{
-	string name;
-	string surname;
-	list<int> ndRez;
-	int egzRez;
-	double average;
-	double med;
-};
-struct buki
-{
-	string name;
-	string surname;
-	list<int> ndRez;
-	int egzRez;
-	double average;
-	double med;
-};
 
-void spauzdinimasFaile(list<mokiniai>&, int, duration<double>&, duration<double>&);
+void spauzdinimasFaile(list<mokiniai>&, list<mokiniai>&, list<mokiniai>&, int, duration<double>&, duration<double>&);
 void spauzdinimasEkrane(list<mokiniai>&, int, duration<double>&);
 void vidurys(list<mokiniai>&, int);
 int skaitymasRanka(list<mokiniai>&, int);
 int skaitymasFailo(list<mokiniai>&, int, duration<double>&, duration<double>&);
 void rikiavimas(list<mokiniai>&, int, char);
-void pazangusIrBuki(list<mokiniai>&, list<pazenge>&, list<buki>&, int, duration<double>&, duration<double>&, duration<double>&);
 int duomenuGeneravimas(list<mokiniai>&, int, duration<double>&);
 int kiekGeneruoti(list<mokiniai>&, int, duration<double>&, duration<double>&);
