34ad5e891728dcb472283add7e6761e54cfd3bef
VytautasVysniauskas <159624996+VytautasVysniauskas@users.noreply.github.com>
2024-04-09 02:28:27 +0300

    Update ManoBibD.h

--- deque/2_strategija/ManoBibD.h
+++ deque/2_strategija/ManoBibD.h
@@ -12,31 +12,12 @@ struct mokiniai
 	double average;
 	double med;
 };
-struct pazenge
-{
-	string name;
-	string surname;
-	deque<int> ndRez;
-	int egzRez;
-	double average;
-	double med;
-};
-struct buki
-{
-	string name;
-	string surname;
-	deque<int> ndRez;
-	int egzRez;
-	double average;
-	double med;
-};
 
-void spauzdinimasFaile(deque<mokiniai>&, int);
+void spauzdinimasFaile(deque<mokiniai>&, deque<mokiniai>&, int);
 void spauzdinimasEkrane(deque<mokiniai>&, int);
 void vidurys(deque<mokiniai>&, int);
 int skaitymasRanka(deque<mokiniai>&, int);
 int skaitymasFailo(deque<mokiniai>&, int, duration<double>&);
 void rikiavimas(deque<mokiniai>&, int, char);
-void pazangusIrBuki(deque<mokiniai>&, deque<buki>&, int, duration<double>&, duration<double>&);
 int duomenuGeneravimas(deque<mokiniai>&, int, duration<double>&);
 int kiekGeneruoti(deque<mokiniai>&, int);
