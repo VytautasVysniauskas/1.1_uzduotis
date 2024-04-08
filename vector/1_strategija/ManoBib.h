41c9e9f81b785f4d906ff7b18065b3a52f7a889d
VytautasVysniauskas <159624996+VytautasVysniauskas@users.noreply.github.com>
2024-04-09 00:49:40 +0300

    Update ManoBib.h

--- vector/1_strategija/ManoBib.h
+++ vector/1_strategija/ManoBib.h
@@ -12,24 +12,6 @@ struct mokiniai
 	double average;
 	double med;
 };
-struct pazenge
-{
-	string name;
-	string surname;
-	vector<int> ndRez;
-	int egzRez;
-	double average;
-	double med;
-};
-struct buki
-{
-	string name;
-	string surname;
-	vector<int> ndRez;
-	int egzRez;
-	double average;
-	double med;
-};
 
 void spauzdinimasFaile(vector<mokiniai>&, int, duration<double>&, duration<double>&);
 void spauzdinimasEkrane(vector<mokiniai>&, int, duration<double>&);
@@ -37,6 +19,6 @@ void vidurys(vector<mokiniai>&, int);
 int skaitymasRanka(vector<mokiniai>&, int);
 int skaitymasFailo(vector<mokiniai>&, int, duration<double>&, duration<double>&);
 void rikiavimas(vector<mokiniai>&, int, char);
-void pazangusIrBuki(vector<mokiniai>&, vector<pazenge>&, vector<buki>&, int, duration<double>&, duration<double>&, duration<double>&);
+void pazangusIrBuki(vector<mokiniai>&, int, duration<double>&, duration<double>&, duration<double>&);
 int duomenuGeneravimas(vector<mokiniai>&, int, duration<double>&);
 int kiekGeneruoti(vector<mokiniai>&, int, duration<double>&, duration<double>&);
