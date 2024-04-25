d599ffceb3285860678b20f61f5b92b180a70c93
VytautasVysniauskas <159624996+VytautasVysniauskas@users.noreply.github.com>
2024-04-25 11:08:29 +0300

    Add files via upload

---
+++ class/ManoBib.h
@@ -0,0 +1,153 @@
+#pragma once
+#include "Bibliotekos.h"
+using namespace std;
+using namespace std::chrono;
+
+class mokiniai {
+private:
+    string name;
+    string surname;
+    vector<int> ndRez;
+    int egzRez;
+    double average;
+    double med;
+
+public:
+    mokiniai() :
+        name(""), surname(""), egzRez(0), average(0), med(0) {}
+
+    mokiniai(const string& n) :
+        name(n), surname(""), egzRez(0), average(0), med(0) {}
+
+    mokiniai(const string& n, const string& s) :
+        name(n), surname(s), egzRez(0), average(0), med(0) {}
+
+    mokiniai(const string& n, const string& s, int egz) :
+        name(n), surname(s), egzRez(egz), average(0), med(0) {}
+
+    mokiniai(const string& n, const string& s, int egz, double av) :
+        name(n), surname(s), egzRez(egz), average(av), med(0) {}
+
+    mokiniai(const string& n, const string& s, int egz, double av, double med) :
+        name(n), surname(s), egzRez(egz), average(av), med(med) {}
+
+    ~mokiniai() 
+    {
+        ndRez.clear();
+    }
+
+    mokiniai(const mokiniai& other) :
+        name(other.name), surname(other.surname), ndRez(other.ndRez),
+        egzRez(other.egzRez), average(other.average), med(other.med) {}
+
+    mokiniai& operator=(const mokiniai& other) 
+    {
+        if (this != &other)
+        {
+            name = other.name;
+            surname = other.surname;
+            ndRez = other.ndRez;
+            egzRez = other.egzRez;
+            average = other.average;
+            med = other.med;
+        }
+        return *this;
+    }
+
+    mokiniai(mokiniai&& other) noexcept :
+        name(std::move(other.name)), surname(std::move(other.surname)),
+        ndRez(std::move(other.ndRez)), egzRez(other.egzRez),
+        average(other.average), med(other.med) { other.clear(); }
+
+    mokiniai& operator=(mokiniai&& other) noexcept 
+    {
+        if (this != &other) 
+        {
+            clear();
+            name = std::move(other.name);
+            surname = std::move(other.surname);
+            ndRez = std::move(other.ndRez);
+            egzRez = other.egzRez;
+            average = other.average;
+            med = other.med;
+            other.clear();
+        }
+        return *this;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const mokiniai& m) 
+    {
+        os << std::setw(15) << std::left << m.surname << std::setw(15) << std::left << m.name
+            << std::setw(10) << std::left << m.egzRez << std::setw(10) << std::left << m.average
+            << std::setw(10) << std::left << m.med << "\n";
+        return os;
+    }
+
+    friend std::istream& operator>>(std::istream& is, mokiniai& m) {
+        std::string line;
+        std::getline(is, line);
+
+        std::stringstream ss(line);
+        std::string name, surname;
+        int egzRez, ndSk;
+        double average, med;
+        ss >> surname >> name >> egzRez >> ndSk;
+
+        m.setName(name);
+        m.setSurname(surname);
+        m.setEgzRez(egzRez);
+
+        for (int i = 0; i < ndSk; ++i) 
+        {
+            int nd;
+            ss >> nd;
+            m.setNdRez(nd);
+        }
+
+        if (ndSk > 0) 
+        {
+            double sum = std::accumulate(m.getNdRez().begin(), m.getNdRez().end(), 0.0);
+            m.setAverage(sum / ndSk, egzRez);
+        }
+
+        std::vector<int> ndRezCopy = m.getNdRez();
+        std::sort(ndRezCopy.begin(), ndRezCopy.end());
+        if (ndSk % 2 == 0)
+        {
+            double vidurys1 = static_cast<double>(ndRezCopy[ndSk / 2]);
+            double vidurys2 = static_cast<double>(ndRezCopy[ndSk / 2 - 1]);
+            m.setMed(vidurys1, vidurys2);
+        }
+        else 
+        {
+            double vidurys = static_cast<double>(ndRezCopy[ndSk / 2]);
+            m.setMed(vidurys);
+        }
+
+        return is;
+    }
+
+    const string& getName() const { return name; }
+    const string& getSurname() const { return surname; }
+    const vector<int>& getNdRez() const { return ndRez; }
+    int getEgzRez() const { return egzRez; }
+    double getAverage() const { return average; }
+    double getMed() const { return med; }
+
+    void setName(const string n) { name = n; }
+    void setSurname(const string s) { surname = s; }
+    void setEgzRez(int egz) { egzRez = egz; }
+    void setNdRez(int nd) { ndRez.push_back(nd); }
+    void setAverage(double ndRez, double egz) { average = 0.4 * ndRez + 0.6 * egz; }
+    void setMed(double vid1, double vid2) { med = (vid1 + vid2) / 2; }
+    void setMed(double vid) { med = vid; }
+};
+
+void spauzdinimasFaile(vector<mokiniai>&, vector<mokiniai>&, int, duration<double>&);
+void spauzdinimasEkrane(vector<mokiniai>&, int);
+void vidurys(vector<mokiniai>&, int);
+int skaitymasRanka(vector<mokiniai>&, int);
+int skaitymasFailo(vector<mokiniai>&, int, duration<double>&);
+void rikiavimas(vector<mokiniai>&, int, char);
+void duomenuGeneravimas(int);
+void kiekGeneruoti();
